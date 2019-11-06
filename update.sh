c1150d2f69347ff277aa27198a24495cf8fc493e
finefoot <33361833+finefoot@users.noreply.github.com>
2019-11-06 13:33:27 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 git config user.name finefoot
 git config user.email finefoot@finefoot.finefoot
 git config --list
+git remote remove origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 git config --list
 # darf auch failen
