a150718a464042a618b5de63a43b1a908ee150c4
finefoot <33361833+finefoot@users.noreply.github.com>
2019-11-06 06:46:34 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,10 @@ import sys
 sys.path.insert(0, os.path.abspath("../src"))
 
 project = "Fibonacci"
-copyright = "2019"
+copyright = "2017, AUTHOR"
+
 extensions = ["sphinx.ext.autodoc"]
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_static_path = ["_static"]
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {"collapse_navigation": True}
-html_static_path = ["_static"]
