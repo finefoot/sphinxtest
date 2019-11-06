dc4e7c19c66a28f26bd6eefd1eaad19aff0078e9
finefoot <33361833+finefoot@users.noreply.github.com>
2019-11-06 04:46:52 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,4 +8,5 @@ extensions = ["sphinx.ext.autodoc"]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {"collapse_navigation": True}
 html_static_path = ["_static"]
